4aec7047544fb3899421512b3a7f65f7deb82099
Abhishek <abhishek.bend@gmail.com>
2014-03-23 20:50:01 -0400

    updating

--- RiderThread.java
+++ RiderThread.java
@@ -26,34 +26,6 @@ public class RiderThread extends Thread {
 			myElevator = myBuilding.CallDown(myFromFloor);
 		}
 
-		// Align the rider with the appropriate event barrier and wait on that until the elevator comes and does OpenDoor to raise the barrier
-		try {
-			eventGettingOn = myBuilding.getOnBarriers[myElevator.elevatorId][myFromFloor];
-			eventGettingOn.arrive(); // wait on barrier
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		// Once the door is open, the rider can enter - the OpenDoor() should result in the eventGettingOn barrier to raise()
-
-		try {
-			if(!myElevator.Enter())
-				/// ENTER CODE - if false, wait for the elevator to leave
-				// another event barrier specific for failing to enter the elevator due to capacity issue should wait on the 
-			eventGettingOn.complete();
-			myElevator.RequestFloor(myToFloor);	
-					// WHEN DOES SLEEP HAPPEN?
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-		try {
-			eventGettingOff = myBuilding.getOffBarriers[myToFloor][myElevator.elevatorId];
-			eventGettingOff.arrive();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
+		// Align the rider with the appropriate event barrier and wait on that
 	}
 }
